97cc6317ccc7acd6e21347b4aadd02b7116bf52a
benhus8 <benedykt.huszcza@gmail.com>
2023-10-04 19:32:03 +0200

    add first endpoint - create person

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Person(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField()
