ebb2dd46a0042a1632cc57fd2d6db4c861e6eaa2
benhus8 <benedykt.huszcza@gmail.com>
2023-10-12 20:47:31 +0200

    migration

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Person(models.Model):
-    number = models.AutoField(primary_key=True)
+    number = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=200)
     email = models.EmailField()
 class ContactRequest(models.Model):
