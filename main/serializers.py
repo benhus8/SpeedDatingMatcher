9e15599795317acf13f10370a75f0ab848ee1675
Mattias988 <maciek.korsakowski@gmail.com>
2023-11-23 20:29:01 +0100

    change serializers

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
+
 from .models import Person
 
 
-class PersonSerializer(serializers.ModelSerializer):
+class PersonCreateSerializer(serializers.ModelSerializer):
+    def validate_number(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("NUMBER_MUST_BE_GREATER_THAN_ZERO")
+        return value
+
     class Meta:
         model = Person
         fields = ['number', 'first_name', 'email']
-        read_only_fields = ['number']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Person.objects.all(),
+                fields=['email'],
+                message= "EMAIL_MUST_BE_UNIQUE"
+            )
+        ]
+
+class PersonUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['first_name', 'email']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Person.objects.all(),
+                fields=['email']
+            )
+        ]
