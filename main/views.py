5ad61fd04dd8d0d4e3db2e670f093ae2f4b983e7
Mattias988 <maciek.korsakowski@gmail.com>
2023-12-07 21:03:07 +0100

    add view

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 
 from .models import Person
-from .serializers import PersonCreateSerializer, PersonUpdateSerializer
+from .serializers import PersonCreateSerializer, PersonUpdateSerializer, PersonDeleteSerializer
+
 
 class PersonCreateView(generics.ListCreateAPIView):
     queryset = Person.objects.all()
@@ -10,3 +11,7 @@ class PersonCreateView(generics.ListCreateAPIView):
 class PersonUpdateView(generics.UpdateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonUpdateSerializer
+
+class PersonDeleteView(generics.DestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonDeleteSerializer
