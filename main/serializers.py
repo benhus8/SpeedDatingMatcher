b01df9d86a046d9527ea85fefd7b029880adee0c
Mattias988 <maciek.korsakowski@gmail.com>
2023-12-07 21:02:50 +0100

    add serializer

--- main/serializers.py
+++ main/serializers.py
@@ -31,3 +31,8 @@ class PersonUpdateSerializer(serializers.ModelSerializer):
                 fields=['email']
             )
         ]
+
+class PersonDeleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['number', 'first_name', 'email']
