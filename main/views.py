bec97b32c00fd3d9d535ae8c71d972d173752d40
Mattias988 <maciek.korsakowski@gmail.com>
2023-12-20 12:48:37 +0100

    add get view

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 
-from .models import Person
-from .serializers import PersonCreateSerializer, PersonUpdateSerializer
+from .models import Person, ContactRequest
+from .serializers import PersonCreateSerializer, PersonUpdateSerializer, GetAllPersonsWithContactsReqSerializer
+
 
 class PersonCreateView(generics.ListCreateAPIView):
     queryset = Person.objects.all()
@@ -10,3 +11,7 @@ class PersonCreateView(generics.ListCreateAPIView):
 class PersonUpdateView(generics.UpdateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonUpdateSerializer
+
+class GetAllPersonsWithContactsReqView(generics.ListAPIView):
+    queryset = ContactRequest.objects.all()
+    serializer_class = GetAllPersonsWithContactsReqSerializer
