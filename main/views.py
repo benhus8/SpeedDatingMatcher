58327b1ba34bb8733438af10e6521a2f62cee488
benhus8 <benedykt.huszcza@gmail.com>
2024-01-02 20:07:16 +0100

    delete redundant import

--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from django.shortcuts import get_object_or_404
 from .models import Person, ContactRequest
 from .serializers import ContactRequestCreateSerializer, PersonUpdateSerializer, PersonCreateSerializer
 from rest_framework.response import Response
