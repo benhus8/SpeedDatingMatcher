e16bcd0feb15c7a6890e2c5357014a1fd6c85ac8
Mattias988 <maciek.korsakowski@gmail.com>
2024-01-02 14:48:56 +0100

    add view

--- main/views.py
+++ main/views.py
@@ -1,16 +1,35 @@
+from django.http import JsonResponse
 from rest_framework import generics
-from django.shortcuts import get_object_or_404
+
 from .models import Person, ContactRequest
-from .serializers import ContactRequestCreateSerializer, PersonUpdateSerializer
+from .serializers import PersonCreateSerializer, PersonUpdateSerializer, ContactRequestCreateSerializer, \
+    ContactRequestDeleteSerializer
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+
 class PersonCreateView(generics.ListCreateAPIView):
     queryset = Person.objects.all()
-    serializer_class = ContactRequestCreateSerializer
+    serializer_class = PersonCreateSerializer
 
 class PersonUpdateView(generics.UpdateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonUpdateSerializer
+
+class ContactRequestDeleteView(generics.DestroyAPIView):
+    queryset = ContactRequest.objects.all()
+    serializer_class = ContactRequestDeleteSerializer
+    def delete(self, request, *args, **kwargs):
+        person_requesting_contact_id = self.kwargs.get('person_requesting_contact_id')
+        preferred_person_id = self.kwargs.get('preferred_person_id')
+
+        contact_request = get_object_or_404(ContactRequest,
+                                           person_requesting_contact_id=person_requesting_contact_id,
+                                           preferred_person_id=preferred_person_id)
+
+        contact_request.delete()
+
+        return JsonResponse({'message': 'Contact request deleted successfully'})
 
 class ConstactRequestCreateView(generics.ListCreateAPIView):
     queryset = ContactRequest.objects.all()
