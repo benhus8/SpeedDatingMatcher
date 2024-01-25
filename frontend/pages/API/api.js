68a754aa9fde4418d04586036a7ebb07f7dd5d71
Mattias988 <maciek.korsakowski@gmail.com>
2024-01-25 20:25:29 +0100

    save api

--- frontend/pages/API/api.js
+++ frontend/pages/API/api.js
@@ -1,16 +1,5 @@
-
-export const getContactRequests = async () => {
-    try {
-        const response = await fetch(' http://127.0.0.1:8000/api/contact-requests')
-        const result = await response.json()
-        return result
-    } catch (error) {
-        console.error('Błąd pobierania danych: ', error)
-        throw error
-    }
-}
-async function getData() {
-  const res = await fetch(' http://127.0.0.1:8000/api/contact-requests')
+export async function getData() {
+  const res = await fetch(' http://127.0.0.1:8000/api/persons/contacts/')
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
@@ -24,3 +13,20 @@ export default async function Page() {
 
   return <main></main>
 }
+
+export const deleteRecord = async (personRequestingContactId, preferredPersonId) => {
+  try {
+    const response = await fetch(`http://127.0.0.1:8000/api/delete-contact-requests/${personRequestingContactId}/${preferredPersonId}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      throw new Error('Nie udało się usunąć rekordu.');
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Błąd podczas usuwania rekordu:', error);
+    throw error;
+  }
+};
