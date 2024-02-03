090011047cadb78a79baf7f945afe9e1b8aa83c7
benhus8 <benedykt.huszcza@gmail.com>
2024-02-03 21:25:46 +0100

    extend models

--- main/models.py
+++ main/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from verify_email import verify_email
 class Person(models.Model):
     number = models.PositiveIntegerField(primary_key=True)
     first_name = models.CharField(max_length=200)
     email = models.EmailField()
-
+    email_verified = models.BooleanField(default=False)
 
 class ContactRequest(models.Model):
     person_requesting_contact = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='person_req_contact')
