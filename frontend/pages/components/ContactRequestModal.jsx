96e872497922ababeb7edae57212833efd7fb349
benhus8 <benedykt.huszcza@gmail.com>
2024-02-07 18:07:39 +0100

    refactor create contact request

--- frontend/pages/components/ContactRequestModal.jsx
+++ frontend/pages/components/ContactRequestModal.jsx
@@ -1,111 +1,106 @@
-import React, { useState } from "react";
+import React, {useMemo, useState} from "react";
 import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  Dropdown,
-  DropdownItem,
-  DropdownTrigger,
-  DropdownMenu
+    Button,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownTrigger,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader
 } from "@nextui-org/react";
-import { createContactRequest } from "../API/api";
+import {createContactRequest} from "../API/api";
 
 export default function ContactRequestModal({
-  contactRequestObject,
-  setContactRequestObject,
-  fetchDataOnClose,
-  isContactRequestModalOpen,
-  onContactRequestModalClose,
-  fetchContactRequest,
-}) {
-  const [selectedContactIds, setSelectedContactIds] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
+                                                contactRequestObject,
+                                                setContactRequestObject,
+                                                fetchDataOnClose,
+                                                isContactRequestModalOpen,
+                                                onContactRequestModalClose,
+                                                fetchContactRequest,
+                                            }) {
+    const [errorMessage, setErrorMessage] = useState("");
 
-  const handleClose = () => {
-    setSelectedContactIds([]);
-    setContactRequestObject(undefined);
-    onContactRequestModalClose();
-  };
+    const [selectedNumbers, setSelectedNumbers] = useState(new Set([]));
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      if (selectedContactIds.length > 0) {
-        const person_requesting_contact_id = contactRequestObject.number;
-        const requestData = {
-          preferred_persons: selectedContactIds
-        };
-        await createContactRequest(requestData, person_requesting_contact_id);
-        fetchDataOnClose();
-        onContactRequestModalClose();
-        setContactRequestObject(undefined);
-      } else {
-        setErrorMessage("Wybierz numer preferowanej osoby");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const selectedValue = useMemo(() => Array.from(selectedNumbers).join(", ").replaceAll("_", " "), [selectedNumbers]);
+    const handleClose = () => {
+        setSelectedNumbers(new Set([]))
+        setContactRequestObject(undefined)
+        onContactRequestModalClose()
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            if (selectedNumbers.size > 0) {
+                const person_requesting_contact_id = contactRequestObject.number;
 
- return (
-    <Modal
-      isOpen={isContactRequestModalOpen}
-      onClose={handleClose}
-      placement="top-center"
+                const numbersList = [];
+                selectedNumbers.forEach(value => {
+                    numbersList.push(value);
+                });
+
+                const requestData = {
+                    preferred_persons: numbersList
+                };
+                await createContactRequest(requestData, person_requesting_contact_id);
+                fetchDataOnClose();
+                setSelectedNumbers(new Set([]))
+                onContactRequestModalClose();
+                setContactRequestObject(undefined);
+            } else {
+                setErrorMessage("Wybierz numer preferowanej osoby");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (<Modal
+        isOpen={isContactRequestModalOpen}
+        onClose={handleClose}
+        placement="top-center"
     >
-      <ModalContent>
-        <form onSubmit={handleSubmit}>
-          <ModalHeader className="flex flex-col gap-1">
-            Dodaj preferencję
-          </ModalHeader>
-          <ModalBody>
-            <Dropdown>
-              <DropdownTrigger>
-                <Button variant="bordered" className="capitalize">
-                  Wybierz numer preferowanej osoby
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Lista kontaktów"
-                selectionMode="multiple"
-                closeOnSelect={false}
-                disallowEmptySelection
-              >
-                {fetchContactRequest && fetchContactRequest.length > 0 ? (
-                  fetchContactRequest.map((contact) => (
-                    <DropdownItem
-                      key={contact.number}
-                      onClick={() => {
-                        if (selectedContactIds.includes(contact.number)) {
-                          setSelectedContactIds(prevIds => prevIds.filter(id => id !== contact.number));
-                        } else {
-                          setSelectedContactIds(prevIds => [...prevIds, contact.number]);
-                        }
-                      }}
-                    >
-                      {contact.number} - {contact.first_name}
-                    </DropdownItem>
-                  ))
-                ) : (
-                  <DropdownItem disabled>Brak danych</DropdownItem>
-                )}
-              </DropdownMenu>
-            </Dropdown>
-            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-          </ModalBody>
-          <ModalFooter>
-            <Button color="danger" variant="flat" onClick={handleClose}>
-              Anuluj
-            </Button>
-            <Button className="bg-green-200" type="submit">
-              Zapisz
-            </Button>
-          </ModalFooter>
-        </form>
-      </ModalContent>
-    </Modal>
-  );
+        <ModalContent>
+            <form onSubmit={handleSubmit}>
+                <ModalHeader className="flex flex-col gap-1">
+                    Dodaj preferencję
+                </ModalHeader>
+                <ModalBody>
+                    <Dropdown>
+                        <DropdownTrigger>
+                            <Button variant="bordered">
+                                {selectedValue !== "" ? selectedValue : "Wybierz numer preferowanej osoby"}
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu
+                            aria-label="Lista kontaktów"
+                            selectionMode="multiple"
+                            closeOnSelect={false}
+                            disallowEmptySelection
+                            selectedKeys={selectedNumbers}
+                            onSelectionChange={setSelectedNumbers}
+                            className="max-h-56 overflow-y-auto"
+                        >
+                            {fetchContactRequest && fetchContactRequest.length > 0 ? (fetchContactRequest.map((contact) => (
+                                <DropdownItem key={contact.number}>
+                                    {contact.number} - {contact.first_name}
+                                </DropdownItem>))) : (<DropdownItem disabled>Brak danych</DropdownItem>)}
+                        </DropdownMenu>
+                    </Dropdown>
+                    {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>}
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="danger" variant="flat" onClick={handleClose}>
+                        Anuluj
+                    </Button>
+                    <Button className="bg-green-200" type="submit">
+                        Zapisz
+                    </Button>
+                </ModalFooter>
+            </form>
+        </ModalContent>
+    </Modal>);
 }
