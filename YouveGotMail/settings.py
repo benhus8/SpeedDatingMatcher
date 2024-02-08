ae60d74f37d0dbf5a9bf75bddf78bd703b1fdc44
benhus8 <benedykt.huszcza@gmail.com>
2024-02-08 23:32:36 +0100

    refactor static files

--- YouveGotMail/settings.py
+++ YouveGotMail/settings.py
@@ -29,6 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_COOKIE_SECURE = False
 
 # Application definition
 
@@ -126,10 +127,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -156,4 +157,3 @@ REST_FRAMEWORK = {
     ),
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
