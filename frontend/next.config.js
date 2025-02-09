1631dd7dde0e1a63d5c7cd88b2163ccf35ba23c0
benhus8 <benedykt.huszcza@gmail.com>
2025-02-09 20:41:02 +0100

    refactor next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 const path = require('path')
 
 module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/:path*',
+            destination: 'http://localhost:8000/:path*',
+          },
+        ]
+      },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
