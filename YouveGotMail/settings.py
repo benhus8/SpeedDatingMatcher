e60bbfe8c60b3adda81e0374339e150650601ac6
benhus8 <benedykt.huszcza@gmail.com>
2026-02-02 17:58:59 +0100

    fix: set DEBUG to False for production readiness

--- YouveGotMail/settings.py
+++ YouveGotMail/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-(tf28#b#6ipm0_0n+v&3dv&37x4-&&s4v0y@1nfv3t-pa6q9!_
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Enable DEBUG for local development to serve static files and detailed errors.
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
